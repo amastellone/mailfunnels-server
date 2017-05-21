ce3ae1ac010e173755986eee189e76c130e4ba28
Matt Twardowski <mttwardowski@gmail.com>
2017-05-21 15:37:12 -0500

    Removed attributes from model on views

--- app/models/rest_model.rb
+++ app/models/rest_model.rb
@@ -2,7 +2,7 @@ require 'active_resource'
 
 class RestModel < ActiveResource::Base
 	self.include_format_in_path = false
-	self.site = "http://localhost:3001"
+	self.site = "http://localhost:3001/api"
 
 	# TODO: SECURITY: Setup JWT or SSL for Client-Server auth
 
