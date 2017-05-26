21f1aa4c9391a2f24b04507f952eeb10cbc37fcd
karolgrycuk <kgrycu2@uic.edu>
2017-05-26 13:31:28 -0500

    webhook processing

--- app/jobs/carts_update_job.rb
+++ app/jobs/carts_update_job.rb
@@ -8,30 +8,35 @@ class CartsUpdateJob < ActiveJob::Base
 
       app = MailfunnelsUtil.get_app
 
+
+      puts "----looking for subscriber----"
       subscriber = Subscriber.where(app_id: app.id, email: "FoofeeNigga@worldstarthiphop.com")
+      puts "----checking for subscriber----"
 
       if subscriber.any? == false
-        puts "----CREATED SUBSCRIBER"
+        puts "----CREATED SUBSCRIBER----"
         Subscriber.create(app_id: app.id, email: "FoofeeNigga@worldstarthiphop.com")
       end
-      hook = Hook.where(identifier: 'cart_update').first
 
-      trigger = Trigger.where(app_id: app.id, hook_id: hook.id)
-      puts "----WE ARE READY TO QUEUE SIDEKIQ JOB 2"
-      if trigger.any?
-        puts "----WE ARE READY TO QUEUE SIDEKIQ JOB 3"
-        puts
+
+      puts "----looking for trigger----"
+      trigger = Trigger.where(app_id: app.id, hook_id: '2').first
+      puts "----looking for trigger----"
+      if trigger.nil? == false
+        puts "----Trigger Found----"
+
+        puts "----looking for funnel----"
         funnel = Funnel.where(app_id: app.id, trigger_id: trigger.id).first
-        if funnel.any?
-          puts "----WE ARE READY TO QUEUE SIDEKIQ JOB 4"
+        puts "----checking for funnel----"
+        if funnel.nil? == false
+          puts "----Funnel Found----"
           puts funnel.id.to_s
           link = Link.where(funnel_id: funnel.id, start_link: 1).first
-          if link.any?
-            puts "----WE ARE READY TO QUEUE SIDEKIQ JOB 5"
-            puts link.id.to_s
+          if link.nil? == false
+            puts "----Link Found----"
             node = link.to_node_id
 
-            puts "----WE ARE READY TO QUEUE SIDEKIQ JOB 6"
+            puts "----WE ARE READY TO QUEUE SIDEKIQ JOB----"
           end
 
         end
