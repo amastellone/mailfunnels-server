2e3b248fb51c59eff21988e0f300225cf78b97ea
Luke Helminiak <l.helminiak@yahoo.com>
2017-05-26 16:42:41 -0500

    Commit for heroku

---
+++ public/assets/pages/allsubscribers-9b9c638bc83f76d8c3b40bf467c215ab8d24d254858146417a4f43add81f05c5.js
@@ -0,0 +1,58 @@
+
+
+$(function() {
+
+    /* --- AUTHENTICATION --- */
+    var csrf_token = $('meta[name=csrf-token]').attr('content');
+
+    /* --- APP VALUES --- */
+    var app_id = $('#current_app_id').val();
+
+    /* --- MODALS --- */
+    var new_subscriber_modal = $('#new_subscriber_modal');
+
+    /* --- INPUT FIELDS --- */
+    var first_name_input = $('#first_name_input');
+    var last_name_input = $('#last_name_input');
+    var email_input = $('#email_input');
+
+    /* --- BUTTONS --- */
+    var new_subscriber_submit_button = $('#new_subscriber_submit_button');
+
+
+
+    new_subscriber_submit_button.on('click', function() {
+
+        var first_name = first_name_input.val();
+        var last_name = last_name_input.val();
+        var email = email_input.val();
+
+        console.log(first_name + " " + last_name + " " + email);
+
+        $.ajax({
+            type:'POST',
+            url: '/ajax_create_subscriber',
+            dataType: "json",
+            data: {
+                app_id: app_id,
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                authenticity_token: csrf_token
+            },
+            error: function(e) {
+                console.log(e);
+            },
+            success: function(response) {
+                console.log(response);
+                new_subscriber_modal.modal('toggle');
+            }
+        });
+
+
+
+
+    });
+
+
+});
