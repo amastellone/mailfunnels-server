a1e60f1518bc9cca6a12f6a72550fd48c330fb02
karolgrycuk <kgrycu2@uic.edu>
2017-05-28 18:21:21 -0500

    webhooks again

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -3,7 +3,6 @@ class Subscriber < RestModel
   belongs_to :app,  :class_name => 'App', :foreign_key => 'app_id'
 
   has_many :email_list_subscribers
-  has_many :captured_hooks
   has_many :email_jobs
 
 end
