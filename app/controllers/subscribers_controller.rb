200cca619adbbc8b682b00eaa63584cd8c816de2
Matt Twardowski <mttwardowski@gmail.com>
2017-05-30 17:50:18 -0500

    ready to push to heroku server

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -83,6 +83,6 @@ class SubscribersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def subscriber_params
-      params.require(:subscribers).permit(:first_name, :last_name, :revenue, :email, :app_id)
+      params.require(:subscribers).permit(:first_name, :last_name, :revenue, :email, :app_id, :created_at)
     end
 end
