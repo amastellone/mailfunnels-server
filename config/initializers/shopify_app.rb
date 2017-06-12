c4dbe6085a376fd9f6858ae508713973b72df1a1
Matt Twardowski <mttwardowski@gmail.com>
2017-06-12 11:24:38 -0500

    Initial Development Commit

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -9,11 +9,11 @@ ShopifyApp.configure do |config|
   config.embedded_app = true
 
   config.webhooks = [
-    {topic: 'refunds/create', address: 'https://www.mailfunnels.com/webhooks/refunds_create', format: 'json'},
-    {topic: 'orders/create', address: 'https://www.mailfunnels.com/webhooks/orders_create', format: 'json'},
-    {topic: 'carts/update', address: 'https://www.mailfunnels.com/webhooks/carts_update', format: 'json'},
-    # {topic: 'refunds/create', address: 'https://cc8c8c32.ngrok.io/webhooks/refunds_create', format: 'json'},
-    # {topic: 'orders/create', address: 'https://cc8c8c32.ngrok.io/webhooks/orders_create', format: 'json'},
-    # {topic: 'carts/update', address: 'https://cc8c8c32.ngrok.io/webhooks/carts_update', format: 'json'},
+    # {topic: 'refunds/create', address: 'https://www.mailfunnels.com/webhooks/refunds_create', format: 'json'},
+    # {topic: 'orders/create', address: 'https://www.mailfunnels.com/webhooks/orders_create', format: 'json'},
+    # {topic: 'carts/update', address: 'https://www.mailfunnels.com/webhooks/carts_update', format: 'json'},
+    {topic: 'refunds/create', address: 'https://eaa5d5a1.ngrok.io/webhooks/refunds_create', format: 'json'},
+    {topic: 'orders/create', address: 'https://eaa5d5a1.ngrok.io/webhooks/orders_create', format: 'json'},
+    {topic: 'carts/update', address: 'https://eaa5d5a1.ngrok.io/webhooks/carts_update', format: 'json'},
   ]
 end
