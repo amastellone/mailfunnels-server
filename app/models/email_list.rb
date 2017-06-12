09e84dffa45a9737e5eb9db5d679cad331618bc3
Matt Twardowski <mttwardowski@gmail.com>
2017-06-12 11:36:12 -0500

    Added Relationship to Unsubscribers and Email Lists in models

--- app/models/email_list.rb
+++ app/models/email_list.rb
@@ -2,7 +2,9 @@ class EmailList < RestModel
 
 	belongs_to :app, :class_name => 'App', :foreign_key => 'app_id'
 
+
   has_many :email_list_subscribers
   has_many :email_jobs
+  has_many :unsubscribers
 
 end
