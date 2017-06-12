1094c13e28987c5754cb8e694bb14dff773bf69e
karolgrycuk <kgrycu2@uic.edu>
2017-06-12 15:38:51 -0500

    unsubscriber add email list id

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -25,13 +25,14 @@ class SubscribersController < ApplicationController
       render html: "You have already been unsibscribed"
 
     else
-
+      email_list_subscriber = EmailListSubscriber.where(subsriber_id: subscriber.id).first
       # Create New Unsubscriber
       unsubscriber = Unsubscriber.new
       unsubscriber.app_id = subscriber.app_id
       unsubscriber.first_name = subscriber.first_name
       unsubscriber.last_name = subscriber.last_name
       unsubscriber.email = subscriber.email
+      unsubscriber.email_list_id = email_list_subscriber.email_list_id
 
       # If error saving unsubscriber, then return error response
       if !unsubscriber.save!
