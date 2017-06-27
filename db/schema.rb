438fd9e27307e7cc0f07082c760332a90811fefe
Matt Twardowski <mttwardowski@gmail.com>
2017-06-27 15:02:51 -0500

    Added First and Last Name to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627182153) do
+ActiveRecord::Schema.define(version: 20170627195521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170627182153) do
     t.string   "zip"
     t.datetime "created_at",     :null=>false
     t.datetime "updated_at",     :null=>false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   create_table "apps", force: :cascade do |t|
