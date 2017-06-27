b0d151803b6375b4122dc00f2f480680d5ab8f9d
Matt Twardowski <mttwardowski@gmail.com>
2017-06-27 12:21:35 -0500

    Changed Client Tag From Integer to String

--- db/migrate/20170623172603_add_client_info_to_apps.rb
+++ db/migrate/20170623172603_add_client_info_to_apps.rb
@@ -1,6 +1,6 @@
 class AddClientInfoToApps < ActiveRecord::Migration[5.0]
   def change
     add_column :apps, :clientid, :integer
-    add_column :apps, :client_tag, :integer
+    add_column :apps, :client_tag, :string
   end
 end
