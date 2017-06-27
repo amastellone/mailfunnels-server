6a1b5c639b5ca3352176769c3ffa4b904d61846e
Matt Twardowski <mttwardowski@gmail.com>
2017-06-27 13:05:23 -0500

    Created User Model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :verify_mf_integrity
 
 
-
   def verify_mf_integrity
 
     begin
@@ -19,9 +18,6 @@ class ApplicationController < ActionController::Base
 
         # Load Account Data from Infusionsoft
         contact = Infusionsoft.data_load('Contact', app.clientid, [:FirstName, :LastName, :Email, :Website, :StreetAddress1, :City, :State, :Groups])
-
-        logger.info "DISPLAYING INFUSIONSOFT DATA:"
-        logger.info contact
 
         # Parse through Tags and look for failed payment tag
         tags = contact['Groups'].split(",")
@@ -59,29 +55,6 @@ class ApplicationController < ActionController::Base
 
   end
 
-
-  def install_server_app
-
-    begin
-      domain = params[:shop]
-
-      if domain != nil
-
-        server_app = App.where(name: domain)
-
-        if server_app.any? == false
-
-          digest = OpenSSL::Digest.new('sha256')
-          token = Base64.encode64(OpenSSL::HMAC.digest(digest, ENV['SECRET_KEY_BASE'], domain)).strip
-          server_app = App.create(name: domain, auth_token: token)
-        end
-      end
-    end
-
-
-  rescue => e
-    logger.error e.message
-  end
 
   helper_method :current_shop, :shopify_session
 
