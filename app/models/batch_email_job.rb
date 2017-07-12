a007c4e3f1f9e4d872c541bfdc3c7e7dac4ef644
Matt Twardowski <mttwardowski@gmail.com>
2017-07-12 11:27:19 -0500

    Added has many relationship with batch email jobs and broadcast lists

--- app/models/batch_email_job.rb
+++ app/models/batch_email_job.rb
@@ -3,4 +3,5 @@ class BatchEmailJob < ApplicationRecord
   belongs_to :email_template, :class_name => 'EmailTemplate', :foreign_key => 'email_template_id'
 
   has_many :email_jobs
+  has_many :broadcast_lists
 end
